b85af91b898f578ff371d7d3e14245909f609fac
Thadoe SY <thadoeswanyi@gmail.com>
2020-07-30 20:51:36 -0700

    Update testbench.cpp

--- Device#74163/systemc/testbench.cpp
+++ Device#74163/systemc/testbench.cpp
@@ -14,18 +14,18 @@ int sc_main (int argc, char* argv[]){
   	sc_signal <sc_uint<4>>  o_Q;
   	sc_signal <sc_uint<1>>  RCO;
   	//sc_signal <bool> clk;
-    sc_clock clk ("my_clock",1,0.5, true);
+    	sc_clock clk ("my_clock",1,0.5, true);   // generate clk from sys c
   	int i = 0;
   	 
 	syn_counter syn_counter0 ("SYN_COUNTER");
-  		syn_counter0.clk(clk);
-  		syn_counter0.Ep(Ep);
-  		syn_counter0.Et(Et);
+        syn_counter0.clk(clk);
+  	syn_counter0.Ep(Ep);
+  	syn_counter0.Et(Et);
         syn_counter0.CLEAR(CLEAR);
         syn_counter0.LOAD(LOAD);
         syn_counter0.i_preset(i_preset);
         syn_counter0.o_Q(o_Q);
-  		syn_counter0.RCO(RCO);	
+  	syn_counter0.RCO(RCO);	
   
     
   	sc_trace_file* wf;
@@ -33,14 +33,14 @@ int sc_main (int argc, char* argv[]){
   	wf = sc_create_vcd_trace_file("syn_counter0");
   
   	sc_trace(wf, clk, "clk");
-    sc_trace(wf, Ep, "Ep");
-    sc_trace(wf, Et, "Et");
-    sc_trace(wf, CLEAR, "CLEAR");
-    sc_trace(wf, LOAD, "LOAD");
-    sc_trace(wf, i_preset, "i_preset");
-    sc_trace(wf, o_Q, "o_Q");
-    sc_trace(wf, RCO, "RCO");
-  
+	sc_trace(wf, Ep, "Ep");
+	sc_trace(wf, Et, "Et");
+	sc_trace(wf, CLEAR, "CLEAR");
+	sc_trace(wf, LOAD, "LOAD");
+	sc_trace(wf, i_preset, "i_preset");
+	sc_trace(wf, o_Q, "o_Q");
+	sc_trace(wf, RCO, "RCO");
+
    
   
   // reset pulse 
