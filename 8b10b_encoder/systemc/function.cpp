a0bcff688c85bcaa6ff92ba78e060b0c43968bb3
Thadoe SY <thadoeswanyi@gmail.com>
2020-08-04 16:29:34 -0700

    Update function.cpp

--- 8b10b_encoder/systemc/function.cpp
+++ 8b10b_encoder/systemc/function.cpp
@@ -10,16 +10,15 @@ void DISP_GEN :: count (void){
   }
   curr_disp = count_r - (10 - count_r );
   o_count.write(count_r);
-  cout<< "@" << sc_time_stamp() <<" disp  " <<curr_disp<<endl; // ignore.for debug
-  
+  cout<< "@" << sc_time_stamp() <<" disp  " <<curr_disp<<endl; // ignore.for debug  
 }
 
 void DISP_GEN :: FSM (void){
   //wait (SC_ZERO_TIME);
   while (true){
   curr_state = (i_rdisp == 1)? RD_plus : RD_minus;
-    
-    cout<< "@" << sc_time_stamp() <<" curr state ini " <<curr_state<<endl;
+
+  cout<< "@" << sc_time_stamp() <<" curr state ini " <<curr_state<<endl;
   
   switch (curr_state){
   	
@@ -29,25 +28,21 @@ void DISP_GEN :: FSM (void){
     	} else {
     		curr_state = RD_plus;
     	}
-    	
     	break;
-    
     case RD_plus:
     	if (curr_disp == 0){
     		curr_state = RD_plus; 	
     	} else {
     		curr_state = RD_minus;
     	}
-    	
     	break;
-  
   }
     tc++;
     test_count.write(tc );                 						          // ignore.for debug
     
     cout<< "@" << sc_time_stamp() <<" " <<curr_state<<endl;   	// ignore.for debug
     
-   wait(); // suspend process untill next event 
+    wait(); // suspend process untill next event 
    
   }
    
